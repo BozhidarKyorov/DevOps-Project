7849bb3eec0e96044ea2247b221f083333eebcc9
Bozhidar <bobkatakio@gmail.com>
2025-09-04 21:43:47 +0300

    Update to devops-project.yaml

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -1,5 +1,13 @@
 import psycopg2
+import pytest
 import os
+
+pytestmark = pytest.mark.skipif(
+    os.getenv("SKIP_DB_TESTS") == "1",
+    reason="Skipping DB tests in container, already tested in db-test job"
+)
+
+
 
 DB_URL = os.getenv(
     "DATABASE_URL", "postgresql://testuser:testpass@localhost:5432/testdb"
